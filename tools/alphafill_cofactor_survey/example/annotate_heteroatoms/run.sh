3f25d421732e826d34fc49d954f4bb28a4614f76
Matteo Tiberti <matteo.tiberti@gmail.com>
2025-05-13 10:27:08 +0200

    add run script in the examples

---
+++ tools/alphafill_cofactor_survey/example/annotate_heteroatoms/run.sh
@@ -0,0 +1,2 @@
+module load python
+python ../../annotate_heteroatoms.py -d ../../cofactors_dict.json -c ../find_cofactor/summary_output_unique_heteroatoms.txt
---
+++ tools/alphafill_cofactor_survey/example/find_cofactor/run.sh
@@ -0,0 +1,2 @@
+module load python
+python ../../find_cofactor.py -i uniprotACs.txt -f ../annotate_heteroatoms/all_cofactors.txt -o summary_output
