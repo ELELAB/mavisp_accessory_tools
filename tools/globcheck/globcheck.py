8d004d08cdc18fad1d9ac07e5644e056dfbd7f89
kar0liina <lina.krzesinska@gmail.com>
2025-05-27 13:24:38 +0200

    addressing review 2

--- tools/globcheck/globcheck.py
+++ tools/globcheck/globcheck.py
@@ -14,11 +14,6 @@ from cath_alphaflow.commands.measure_globularity import (
 log.basicConfig(level=log.INFO, \
                 format='%(levelname)s - %(message)s')
 
-# Thresholds
-asphericity_threshold = 0.1
-rg_threshold = 0.356
-pd_threshold = 10.333
-
 def validate_residue_range(pdb_file, trimming_range):
     """Validates that the provided residue trimming range is 
     within the actual residue range present in the PDB structure.
@@ -26,35 +21,46 @@ def validate_residue_range(pdb_file, trimming_range):
     Args:
         pdb_file (str): Path to the input PDB
         trimming_range (str): Residue range in format "start-end" ("1-122")
+    ----------
+    Returns:
+        start_res (int): starting residue of the structure
+        end_res (int): final residue of the structure 
     """
     if not os.path.isfile(pdb_file):
         log.error(f"❌ PDB file '{pdb_file}' does not exist.")
         sys.exit(1)
 
-    try:
-        start_res, end_res = map(int, trimming_range.split('-'))
-    except ValueError:
-        log.error(f"❌ Invalid format for trimming range '{trimming_range}'. Use format e.g. '1-122'.")
-        sys.exit(1)
-
     u = mda.Universe(pdb_file)
 
-    chain_ids = set(res.segid.strip() for res in u.residues if res.segid.strip())
-    if len(chain_ids) != 1:
-        log.error(f"❌ Structure contains multiple chains: {sorted(chain_ids)}. Input single chain only.")
+    if len(u.segments) != 1:
+        log.error(f"❌ Structure contains {len(u.segments)} chains. Input single chain only.")
         sys.exit(1)
 
-    pdb_residue_ids = set(res.resid for res in u.residues)
-    requested_ids = set(range(start_res, end_res + 1))
-    missing_ids = requested_ids - pdb_residue_ids
+    res_ids = sorted(set(res.resid for res in u.residues))
+    if trimming_range:
+        try:
+            start_res, end_res = map(int, trimming_range.split('-'))
+        except ValueError:
+            log.error(f"❌ Invalid format for trimming range '{trimming_range}'. Use format e.g. '1-122'.")
+            sys.exit(1)
 
-    if missing_ids:
-        log.error(f"❌ Residues not found in PDB: {sorted(missing_ids)}")
-        sys.exit(1)
+        res_ids = set(res.resid for res in u.residues)
+        requested_ids = set(range(start_res, end_res + 1))
+        missing_ids = requested_ids - res_ids
+
+        if missing_ids:
+            log.error(f"❌ Residues not found in PDB: {sorted(missing_ids)}")
+            sys.exit(1)
+    else:
+        start_res = res_ids[0]
+        end_res = res_ids[-1]
+        trimming_range = f"{start_res}-{end_res}"
+        log.info(f"No residue range provided. Using full PDB.")
 
     log.info(f"✅ Residue range {trimming_range} and structure are valid.")
+    return start_res, end_res
 
-def calculate_asphericity(pdb_file, trimming_range):
+def calculate_asphericity(pdb_file, start_res, end_res):
     """Calculates the asphericity of the structure using MDAnalysis.
 
     Asphericity is a measure of how non-spherical a structure is,
@@ -62,14 +68,14 @@ def calculate_asphericity(pdb_file, trimming_range):
 
     Args:
         pdb_file (str):Path to the input PDB
-        trimming_range (str): Residue range in format "start-end" ("1-122")
+        start_res (int): Starting residue of the structure
+        end_res (int): Final residue of the structure
 
     Returns:
         float: Asphericity value for the structure
     """
     u = mda.Universe(pdb_file)
 
-    start_res, end_res = map(int, trimming_range.split('-'))
     selection = f"resid {start_res}:{end_res}"
     atoms = u.select_atoms(selection)
     asphericity = atoms.asphericity(pbc=False)
@@ -78,7 +84,7 @@ def calculate_asphericity(pdb_file, trimming_range):
 
     return asphericity
 
-def calculate_globularity_metrics(pdb_file, trimming_range):
+def calculate_globularity_metrics(pdb_file, start_res, end_res):
     """Calculates globularity metrics for a trimmed domain using CATH AlphaFlow.
 
     The metrics include:
@@ -87,13 +93,13 @@ def calculate_globularity_metrics(pdb_file, trimming_range):
 
     Args:
         pdb_file (str): Path to the input PDB
-        trimming_range (str): Residue range in format "start-end" ("1-122")
-
+        start_res (int): Starting residue of the structure
+        end_res (int): Final residue of the structure
     Returns:
         tuple: A tuple (normed_rg, packing_density), both floats
     """
-    chopping = ChoppingPdbResLabel.from_str(f"{trimming_range}")
-
+    chopping_str = f"{start_res}-{end_res}"
+    chopping = ChoppingPdbResLabel.from_str(f"{chopping_str}")
     model_id = os.path.splitext(os.path.basename(pdb_file))[0]
     domain_id = GeneralDomainID(raw_id=model_id, chopping=chopping)
 
@@ -106,7 +112,7 @@ def calculate_globularity_metrics(pdb_file, trimming_range):
 
     return normed_rg, packing_density
 
-def process_results(model_id, asphericity, normed_rg, packing_density, output_file):
+def process_results(model_id, asphericity, normed_rg, packing_density, output_file,  asphericity_threshold, rg_threshold, pd_threshold):
     """Processes results to finalise an output
     file in CSV format. Adding column asserting
     Pass/Fail of thresholds.
@@ -117,6 +123,9 @@ def process_results(model_id, asphericity, normed_rg, packing_density, output_fi
         normed_rg (float): Normalised rg value
         packing_density (float): Packing Density
         output_file (str): Path to output file 
+        asphericity_threshold (float): Asphericity threshold 
+        rg_threshold (float): Normalised Rg threshold
+        pd_threshold (float): Packing density threshold
     """
     result = {
         "model_id": model_id,
@@ -142,20 +151,23 @@ def main():
     warnings.filterwarnings("ignore", category=UserWarning)
     warnings.filterwarnings("ignore", category=DeprecationWarning)
 
-    parser = argparse.ArgumentParser(description="Run globularity metrics using CATH AlphaFlow and MDAnalysis.")
+    parser = argparse.ArgumentParser(description="Run globularity metrics using CATH AlphaFlow and MDAnalysis.", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument("-p", "--pdb", required=True, help="Input trimmed PDB file")
-    parser.add_argument("-r", "--range", required=True, help="Residue trimming range (e.g., 160-214)")
+    parser.add_argument("-r", "--range", help="Residue trimming range (e.g., 160-214)")
     parser.add_argument("-o", "--output", help="Output CSV file")
+    parser.add_argument("-a", "--asphericity", type=float, default=0.1, help=f"Threshold for asphericity")
+    parser.add_argument("-pd", "--packing", type=float, default=10.333, help=f"Threshold for packing density")
+    parser.add_argument("-rg", "--radius", type=float, default=0.356, help=f"Threshold for normalised radius of gyration")
     args = parser.parse_args()
 
     model_id = os.path.splitext(os.path.basename(args.pdb))[0]
 
     output_file = args.output or f"{model_id}_globularity_recap.csv"
 
-    validate_residue_range(args.pdb, args.range)
-    asphericity = calculate_asphericity(args.pdb, args.range)
-    normed_rg, packing_density = calculate_globularity_metrics(args.pdb, args.range)
-    process_results(model_id, asphericity, normed_rg, packing_density, output_file)
+    start_res, end_res = validate_residue_range(args.pdb, args.range)
+    dom_asphericity = calculate_asphericity(args.pdb, start_res, end_res)
+    normed_rg, packing_density = calculate_globularity_metrics(args.pdb, start_res, end_res)
+    process_results(model_id, dom_asphericity, normed_rg, packing_density, output_file, args.asphericity, args.radius, args.packing)
 
 if __name__ == "__main__":
     main()
